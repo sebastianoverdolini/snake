26ecabc75729a3a802adc0c3ca0804ae2cb0c43f
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2022-12-06 21:15:52 +0100

    Replace the CLI with a GUI

--- Snake.java
+++ Snake.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
+
 public final class Snake
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, snake!");
+        final var frame = new JFrame("Snake");
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }

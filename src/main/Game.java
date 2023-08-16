278e86d35af7671a14d51d8c37dd085882e6f96a
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2023-08-16 12:43:50 +0200

    Rename snakeSize to tileSize in renderBackground

--- src/main/Game.java
+++ src/main/Game.java
@@ -35,14 +35,14 @@ public final class Game
     public static Color darkGrassColor = new Color(128, 185, 24);
     public static Color lightGrassColor = new Color(170, 204, 0);
 
-    private static void renderBackground(Graphics g, int frameSize, int snakeSize)
+    private static void renderBackground(Graphics g, int frameSize, int tileSize)
     {
         g.setColor(darkGrassColor);
-        for (int i = 0; i < frameSize; i = i + snakeSize)
+        for (int i = 0; i < frameSize; i = i + tileSize)
         {
-            for (int j = 0; j < frameSize; j = j + snakeSize)
+            for (int j = 0; j < frameSize; j = j + tileSize)
             {
-                g.fillRect(j, i, snakeSize, snakeSize);
+                g.fillRect(j, i, tileSize, tileSize);
                 if (g.getColor().equals(darkGrassColor))
                 {
                     g.setColor(lightGrassColor);

b87c5b23635cc5c67eb8af6ebc77e987edfe698e
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2023-10-15 15:36:00 +0200

    Simplify DirectionTest's structure

--- src/test/DirectionTest.java
+++ src/test/DirectionTest.java
@@ -3,24 +3,20 @@ import java.util.stream.Stream;
 
 public class DirectionTest
 {
-    public static final List<Test> tests = List.of(
-            new Test("Direction's opposite", () ->
-            {
-                record Example(
-                        Direction direction,
-                        Direction oppositeDirection) {}
-                Stream.of(
-                        new Example(Direction.NORTH, Direction.SOUTH),
-                        new Example(Direction.SOUTH, Direction.NORTH),
-                        new Example(Direction.EAST, Direction.WEST),
-                        new Example(Direction.WEST, Direction.EAST))
-                        .map(example ->
-                                new Test(String.format("""
-                                        The opposite of %s is %s
-                                        """, example.direction, example.oppositeDirection), () ->
-                                {
-                                    assert example.direction.opposite() == example.oppositeDirection;
-                                })).forEach(Test::run);
-            })
-    );
+    record Example(
+            Direction direction,
+            Direction oppositeDirection) {}
+
+    public static final List<Test> tests = Stream.of(
+                    new Example(Direction.NORTH, Direction.SOUTH),
+                    new Example(Direction.SOUTH, Direction.NORTH),
+                    new Example(Direction.EAST, Direction.WEST),
+                    new Example(Direction.WEST, Direction.EAST))
+            .map(example ->
+                    new Test(
+                            "The opposite of " + example.direction + " is " + example.oppositeDirection,
+                            () -> Assertions.assertEquals(
+                                    example.direction.opposite(),
+                                    example.oppositeDirection)))
+            .toList();
 }
